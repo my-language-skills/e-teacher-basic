93e7d9ccaedc1a5d4a73f96c0639ae977970e20f
On-lingua.com <colomet@hotmail.com>
2013-11-05 12:55:09 +0100

    charge of the site

---
+++ pagex.php
@@ -0,0 +1,17 @@
+
+<HTML>
+<HEAD>
+<TITLE>page-10.php</TITLE>
+</HEAD>
+<BODY >
+<H1><CENTER><BR/><BR/>ATTENCION!! </H1></CENTER><BR/><BR/>
+<H3><CENTER>If you read this page you don't have changed the School's URL.<BR/><BR/>
+For do this you have to open the file "footer.php" and go at line 27: change "href" reference with your school's URL.<BR/><BR/>
+You can also change the sentence "Visit your School" </CENTER></H3>
+<BR/><BR/><BR/><BR/><BR/>
+<H1><CENTER>
+<A HREF="/" COLOR= RED>OK</A>
+</H1></CENTER>
+</BODY>
+</HTML>
+
